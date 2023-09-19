d636d1c129657c5d0efa2ffe6c292b27315991ed
jaehyunjang <hyuni2020@gmail.com>
2023-09-19 15:43:09 +0900

    Todo list by Redux Toolkit + reducer

--- src/store.js
+++ src/store.js
@@ -1,30 +1,28 @@
 // store.js
 
 import { createStore } from 'redux'
-import { createAction } from '@reduxjs/toolkit'
+import { createAction, createReducer } from '@reduxjs/toolkit'
 
 const addToDo = createAction('ADD')
 const deleteToDo = createAction('DELETE')
 
 const initialState = JSON.parse(localStorage.getItem('todos')) || []
 
-const reducer = (state = initialState, action) => {
-  let newState
-  switch (action.type) {
-    case addToDo.type:
-      newState = [{ text: action.payload, id: Date.now() }, ...state]
-      break
-    case deleteToDo.type:
-      newState = state.filter((toDo) => toDo.id !== action.payload)
-      break
-    default:
-      newState = state
-  }
-  localStorage.setItem('todos', JSON.stringify(newState))
-  return newState
-}
+const reducer = createReducer(initialState, {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() })
+  },
+  [deleteToDo]: (state, action) => {
+    return state.filter((toDo) => toDo.id !== action.payload)
+  },
+})
 
 const store = createStore(reducer)
+
+// 스토어의 상태가 변경될 때마다 localStorage에 저장
+store.subscribe(() => {
+  localStorage.setItem('todos', JSON.stringify(store.getState()))
+})
 
 export const actionCreators = {
   addToDo,
